1070dfef6a823e69ec69ea7ffed3240f5d63bf09
Jbdavis94 <55038871+Jbdavis94@users.noreply.github.com>
2019-09-22 22:35:29 -0400

    Challenge 1 UFO Game
    Challenge 1 of our UFO game

--- UFO Game/Assets/Scripts/PlayerController.cs
+++ UFO Game/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,22 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text CountText;
     public Text winText;
+    public Text loseText;
+    public Text livesText;
 
     private Rigidbody2D rb2d;
     private int count;
+    private int lives;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
+        lives = 3;
         winText.text = "";
+        loseText.text = "";
         SetCountText();
+        SetLivesText();
     }
 
     void FixedUpdate()
@@ -39,14 +45,38 @@ public class PlayerController : MonoBehaviour {
             count = count + 1;
             SetCountText();
         }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+
+        if (count == 12)
+        {
+            transform.position = new Vector2(58.3f, 0.0f); 
+        }
+
+        else if (lives == 0)
+        {
+            Destroy(this);
+        }
     }
 
     void SetCountText()
     {
         CountText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        if (count >= 20)
         {
             winText.text = "You win! Game created by Jasmine Davis!";
+        }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            loseText.text = "You Lose! Better luck next time!";
         }
     }
 }
